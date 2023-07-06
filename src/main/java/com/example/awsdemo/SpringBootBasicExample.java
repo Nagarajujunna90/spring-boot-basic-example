70946986ba638d0a9a76113ce980162f0d362747
njunna <nagarajujunna90@gmail.com>
2023-07-06 12:29:54 +0530

    swagger implementation

--- src/main/java/com/example/awsdemo/SpringBootBasicExample.java
+++ src/main/java/com/example/awsdemo/SpringBootBasicExample.java
@@ -1,14 +1,13 @@
 package com.example.awsdemo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
-
+@OpenAPIDefinition(info = @Info(title = "Spring Boot Basic Application",description = "Spring boot example with out any DB"))
 @SpringBootApplication
-@RestController
 public class SpringBootBasicExample {
-
-
     public static void main(String[] args) {
         SpringApplication.run(SpringBootBasicExample.class, args);
     }
