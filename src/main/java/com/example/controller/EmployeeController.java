12a057136e252cb188aa73076e127ece47fb910c
nagaraju.junna <nagaraju.junna90@gmail.com>
2023-10-21 14:29:12 +0530

    updated method

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -10,7 +10,7 @@ public class EmployeeController {
 
     @GetMapping("/employee")
     public ResponseEntity<?> getEmployee() {
-        return new ResponseEntity<>("Hello world version 4", HttpStatus.CREATED);
+        return new ResponseEntity<>("Latest version Nagaraju", HttpStatus.CREATED);
     }
 
 
