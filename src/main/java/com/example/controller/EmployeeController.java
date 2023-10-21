e106da8a99f91d2a9b6b3fe1c9ed6b2b43adf7f3
nagaraju.junna <nagaraju.junna90@gmail.com>
2023-10-21 19:26:13 +0530

    updated

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -10,7 +10,7 @@ public class EmployeeController {
 
     @GetMapping("/employee")
     public ResponseEntity<?> getEmployee() {
-        return new ResponseEntity<>("Latest version Hammayya", HttpStatus.CREATED);
+        return new ResponseEntity<>("Latest version Hey Updated", HttpStatus.CREATED);
     }
 
 
